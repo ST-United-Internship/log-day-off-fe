d32d7b472f50365ca8f8de2b1070ddbc1753bc68
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2023-04-13 08:53:54 +0700

    feat: ldo-05

--- src/pages/ApproveModal.jsx
+++ src/pages/ApproveModal.jsx
@@ -1,5 +1,19 @@
+import { Button, Form } from "antd";
+import "../assets/approve/approvemodal.css";
 const ApproveModal = () => {
-  return <div>sdfsdfsdfsdfsdfsdfsdfsdfsdfsdf</div>;
+  return (
+    <Form className="form-approve">
+      <div>
+        <p> Are you sure?</p>
+        <br></br>
+        <br></br>
+        <br></br>
+        <Button className="approve-nut-mot">Yes</Button>
+        <Button className="approve-nut-hai" type="primary">
+          No
+        </Button>
+      </div>
+    </Form>
+  );
 };
-
 export default ApproveModal;

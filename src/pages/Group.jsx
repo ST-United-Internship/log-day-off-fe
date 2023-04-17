d0394f405056489b891a5a905ac60f1659a03a98
HieuDinh <hieuldgcd18458@fpt.edu.vn>
2023-04-17 14:27:13 +0700

    fix: loi

---
+++ src/pages/Group.jsx
@@ -0,0 +1,38 @@
+import { Table } from "antd";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Member (s)",
+    dataIndex: "member",
+    defaultSortOrder: "descend",
+  },
+  {
+    title: "Master (s)",
+    dataIndex: "master",
+  },
+];
+const data = [
+  {
+    key: "1",
+    name: "HR",
+    member: "API o day",
+    master: "api o day",
+  },
+  {
+    key: "2",
+    name: "KAS",
+    member: "API o day ",
+    master: "api o day",
+  },
+];
+
+const Group = () => (
+  <>
+    <Table columns={columns} dataSource={data} />
+  </>
+);
+export default Group;

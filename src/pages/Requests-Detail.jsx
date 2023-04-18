2d04ac473cc42bbaf6a180e33a1c199640b8075e
HieuDinh <hieuldgcd18458@fpt.edu.vn>
2023-04-18 17:31:24 +0700

    feat: alert

--- src/pages/Requests-Detail.jsx
+++ src/pages/Requests-Detail.jsx
@@ -1,12 +1,8 @@
 import { Table } from "antd";
 import { Col, Row } from "antd";
-import {
-  EditOutlined,
-  CheckOutlined,
-  CloseOutlined,
-  ReloadOutlined,
-} from "@ant-design/icons";
-import { Button } from "antd";
+import { useState } from "react";
+import { EditOutlined, CheckOutlined, CloseOutlined } from "@ant-design/icons";
+import { Button, Modal, Input } from "antd";
 import "../assets/css/requests.css";
 import withAuthorization from "../HOCs/withAuthorization";
 import { ROLE } from "../constants/roles";
@@ -51,6 +47,42 @@ const TableExample = () => {
   const tableStyle = {
     width: "90%",
   };
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const showModal = () => {
+    setOpen(true);
+  };
+  const handleOk = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setOpen(false);
+    }, 1000);
+  };
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  const [modal2Open, setModal2Open] = useState(false);
+
+  const [loading1, setLoading1] = useState(false);
+  const [open1, setOpen1] = useState(() => {
+    return <Input.TextArea></Input.TextArea>;
+  });
+  const showModal1 = () => {
+    setOpen1(true);
+  };
+  const handleOk1 = () => {
+    setLoading1(true);
+    setTimeout(() => {
+      setLoading1(false);
+      setOpen1(false);
+    }, 1000);
+  };
+  const handleCancel1 = () => {
+    setOpen1(false);
+  };
+
   return (
     <>
       <Row>
@@ -68,18 +100,70 @@ const TableExample = () => {
               className="iconre"
               type="primary"
               icon={<CheckOutlined />}
+              onClick={showModal}
             />
+            <Modal
+              open={open}
+              title="Are you sure ????  "
+              onOk={handleOk}
+              onCancel={handleCancel}
+              footer={[
+                <Button key="back" onClick={handleCancel}>
+                  Cancel
+                </Button>,
+                <Button
+                  key="Approve"
+                  type="primary"
+                  loading={loading}
+                  onClick={handleOk}
+                >
+                  Approve
+                </Button>,
+              ]}
+            ></Modal>
+
             <Button
-              className="iconre"
+              className="iconre1"
               type="primary"
               icon={<CloseOutlined />}
+              onClick={() => setModal2Open(true)}
             />
+            <Modal
+              title="Are you sure ???"
+              centered
+              open={modal2Open}
+              onOk={() => setModal2Open(false)}
+              onCancel={() => setModal2Open(false)}
+            ></Modal>
+
             <Button
-              className="iconre"
+              className="iconre2"
               type="primary"
-              icon={<ReloadOutlined />}
+              icon={<EditOutlined />}
+              onClick={showModal1}
             />
-            <Button className="iconre" type="primary" icon={<EditOutlined />} />
+            <Modal
+              open={open1}
+              title="Are you sure ????  "
+              onOk={handleOk1}
+              onCancel={handleCancel1}
+              footer={[
+                <p>
+                  <Input.TextArea></Input.TextArea>
+                </p>,
+                <Button key="back" onClick={handleCancel1}>
+                  Cancel
+                </Button>,
+                <Button
+                  key="Approve"
+                  type="primary"
+                  loading={loading1}
+                  onClick={handleOk1}
+                >
+                  send
+                </Button>,
+              ]}
+            ></Modal>
           </div>
         </Col>
         <Col span={12}>

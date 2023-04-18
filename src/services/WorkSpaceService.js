36ecb7ba28686a669b0d371c4a85fe9f5af3ef8e
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2023-04-18 15:59:03 +0700

    feat: update create workspace

--- src/services/WorkSpaceService.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import { API_URL } from "../constants/url";
-
-export const createWorkSpace = async (data) => {
-  const res = await axios.post(API_URL.CREATE_WORKSPACE, data);
-  return res.data;
-};

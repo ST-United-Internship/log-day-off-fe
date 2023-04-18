12944caa3a05714756bb4b5488ea2a76448e33d8
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2023-04-18 11:10:30 +0700

    feat: ldo-05 update

--- src/routes/privateRoutes.jsx
+++ src/routes/privateRoutes.jsx
@@ -1,17 +1,8 @@
 import PrivateLayout from "../components/PrivateLayout/PrivateLayout";
 import Dashboard from "../pages/Dashboard";
 import DayOff from "../pages/DayOff";
-import Forbidden from "../pages/Forbidden/Forbidden";
-import FormUser from "../pages/FormUser";
 import Request from "../pages/Request";
-<<<<<<< HEAD
-=======
-import ApproveModal from "../components/RequestForm/ApproveModal";
-import RejectModal from "../components/RequestForm/RejectModal";
-import RequestChange from "../components/RequestForm/RequestChange";
->>>>>>> 72e270f (feat: ldo-05 fix)
 
-import Notification from "../pages/Notification";
 const privateRoutes = [
   {
     element: <PrivateLayout />,

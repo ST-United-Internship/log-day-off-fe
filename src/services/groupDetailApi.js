1e1039c7af3aba3cb314f630e3b0c36d66093dd5
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2023-04-21 11:46:25 +0700

    feat: ldo-12 call api (#60)

--- src/services/groupDetailApi.js
+++ src/services/groupDetailApi.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { API_URL } from "../constants/url";
 
-export const addAssignMember = (id, userId) =>
-  axios.post(
-    API_URL.WORKSPACE_DETAIL + "/" + id + "/" + API_URL.ADDASSIGN_USERS,
-    {
-      userId,
-    }
-  );
+export const addAssignMember = (userId, groupId) =>
+  axios.post(API_URL.GROUP + "/" + API_URL.ASSIGN_MEMBER, {
+    groupId,
+    userId,
+  });

84cbe449b7daad03240ffa95d3b6f4b990657754
phuoctn412 <59587426+ngocphuocha@users.noreply.github.com>
2023-04-21 11:49:28 +0700

    fix: fix button loading export google sheet (#61)
    Co-authored-by: de_tu_anh_thanh <phuoctn412@gmail.com>

--- src/pages/ExportGoogleSheet/ExportDayoff.jsx
+++ src/pages/ExportGoogleSheet/ExportDayoff.jsx
@@ -2,10 +2,13 @@ import { Button } from "antd";
 import { useExportDayOffData } from "../../hooks/useExportDayOffData";
 
 const ExportDayoff = () => {
-  const { mutate: exportDayoff } = useExportDayOffData();
+  const { mutate: exportDayoff, isLoading: exportLoading } =
+    useExportDayOffData();
   return (
     <div>
-      <Button onClick={exportDayoff}>Export now</Button>
+      <Button loading={exportLoading} onClick={exportDayoff}>
+        Export now
+      </Button>
     </div>
   );
 };

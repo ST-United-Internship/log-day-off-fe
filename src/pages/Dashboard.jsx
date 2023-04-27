e567ae3775ee775c9b237a1098b9fd95a9e44ba8
HoangNhi152 <85232487+HoangNhi152@users.noreply.github.com>
2023-04-27 17:49:36 +0700

    feat: update backgroud img (#116)
    Co-authored-by: nnhi9428 <nnhi9428@gmail.com>

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,25 +1,19 @@
 const Dashboard = () => {
   const img =
-    "https://icdn.dantri.com.vn/thumb_w/680/2023/01/24/khoa-hocdocx-1674520013659.png";
+    "https://img3.thuthuatphanmem.vn/uploads/2019/07/07/anh-meo-con-de-thuong_020434420.jpg";
   return (
-    <div>
-      <div
-        className="dashboard-container"
-        style={{ width: "1000px", height: "1000px" }}
-      >
-        <div
-          style={{
-            backgroundImage: `url(${img})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-            height: "100%",
-            width: "100%",
-            zIndex: 9999,
-          }}
-        ></div>
-      </div>
-    </div>
+    <div
+      className="dashboard-container"
+      style={{
+        backgroundImage: `url(${img})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        height: "100%",
+        width: "100%",
+        zIndex: 9999,
+      }}
+    ></div>
   );
 };
 

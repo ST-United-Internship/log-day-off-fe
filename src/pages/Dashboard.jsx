acbbac3c6c2ca26fd7aaff4bf0fb15a87cd329cc
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2023-04-27 17:13:32 +0700

    feat: update ui dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,26 @@
-import { ROLE } from "../constants/roles";
-import withAuthorization from "../HOCs/withAuthorization";
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const img =
+    "https://icdn.dantri.com.vn/thumb_w/680/2023/01/24/khoa-hocdocx-1674520013659.png";
+  return (
+    <div>
+      <div
+        className="dashboard-container"
+        style={{ width: "1000px", height: "1000px" }}
+      >
+        <div
+          style={{
+            backgroundImage: `url(${img})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            height: "100%",
+            width: "100%",
+            zIndex: 9999,
+          }}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
-export default withAuthorization([ROLE.ADMIN])(Dashboard);
+export default Dashboard;
